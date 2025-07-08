2a4c5e35f2e306dc4ce9014b493c15d879d89b4c
javiRodrigue <agudtinjava@gmail.com>
2025-07-07 21:52:50 -0300

    agregando funcionalidad de que se abra automaticamente la app

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // 👉 esto abre el navegador automáticamente
+    port: 5173  // podés cambiarlo si querés
+  }
 })

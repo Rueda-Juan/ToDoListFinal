7d4962202b1855bab6e4db53e233e6ad8ec260ab
Rueda-Juan <juanchirueda3@gmail.com>
2025-07-08 22:19:16 -0300

    mejorado de botones

--- Frontend/src/pages/CrearTareaModal.jsx
+++ Frontend/src/pages/CrearTareaModal.jsx
@@ -4,10 +4,10 @@ function CrearTareaModal({ mostrar, onClose, onSubmit, titulo, setTitulo, descri
   return (
     <div className="modal d-block fade show" tabIndex="-1" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
       <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content glass shadow">
+        <div className="modal-content glass">
           <div className="modal-header">
             <h5 className="modal-title">Crear nueva tarea</h5>
-            <button type="button" className="btn-close glass" onClick={onClose}><i class="bi bi-x"></i></button>
+            <button type="button" className="btn-close glass btn-glass-red" onClick={onClose}><i class="bi bi-x"></i></button>
           </div>
           <div className="modal-body">
             <form onSubmit={onSubmit}>
@@ -30,10 +30,10 @@ function CrearTareaModal({ mostrar, onClose, onSubmit, titulo, setTitulo, descri
                 ></textarea>
               </div>
               <div className="modal-footer">
-                <button type="button" className="btn glass" onClick={onClose}>
+                <button type="button" className="btn glass btn-glass-red" onClick={onClose}>
                   Cancelar
                 </button>
-                <button type="submit" className="btn glass">
+                <button type="submit" className="btn glass btn-glass-green">
                   Crear tarea
                 </button>
               </div>
